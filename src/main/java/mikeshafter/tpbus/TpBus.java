3210ef8a92378d7e780f807e41b28d1d8eb1166f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-06 19:56:40 +0900

    1.16.3r2

--- src/main/java/mikeshafter/tpbus/TpBus.java
+++ src/main/java/mikeshafter/tpbus/TpBus.java
@@ -6,6 +6,9 @@ import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.WallSign;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,16 +16,21 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-public final class TpBus extends JavaPlugin implements Listener{
+
+public final class TpBus extends JavaPlugin implements Listener, CommandExecutor{
   @Override
   public void onEnable() {
-    getConfig().options().copyDefaults(true);
-    saveConfig();
-    getServer().getPluginManager().registerEvents(this,this); }
+    getConfig().options().copyDefaults(true); saveConfig();
+    getServer().getPluginManager().registerEvents(this,this);
+  }
+
   @Override
   public void onDisable() {
     saveConfig();
-    getServer().getConsoleSender().sendMessage(ChatColor.AQUA+"TpBus Plugin has been disabled!"); }
+    getServer().getConsoleSender().sendMessage(ChatColor.AQUA+"TpBus Plugin has been disabled!");
+  }
+  
+
   @EventHandler
   public void onSignClick(PlayerInteractEvent event) {
     if (event.getClickedBlock() != null){
@@ -37,8 +45,7 @@ public final class TpBus extends JavaPlugin implements Listener{
           int stop = Integer.parseInt(sign.getLine(2));
           FileConfiguration config = this.getConfig();
           Location location = block.getLocation();
-          config.set(route+"."+stop, location);
-          saveConfig();
+          config.set(route+"."+stop, location); saveConfig();
           location = (Location) config.get(route+"."+(stop+1));
           if (location != null){
             location.setX(location.getX()+0.5);
@@ -46,4 +53,27 @@ public final class TpBus extends JavaPlugin implements Listener{
             player.sendMessage(ChatColor.YELLOW+"Route name: "+route+". Next stop: Bus stop no. "+(stop+1));
             player.teleport(location);
             location.setX(location.getX()-0.5);
-            location.setZ(location.getZ()-0.5); }}}}}}
+            location.setZ(location.getZ()-0.5); }
+          else {player.sendMessage(ChatColor.YELLOW+"Registered Bus Stop "+stop+" on route "+route+".");
+          }
+        }
+      }
+    }
+  }
+  
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+    try {
+      String route = args[0];
+      int stop = Integer.parseInt(args[1]);
+      FileConfiguration config = this.getConfig();
+      config.set(route+"."+stop, null);
+      saveConfig();
+      sender.sendMessage(ChatColor.YELLOW+"Bus Stop "+stop+" on route "+route+" has been unregistered.");
+      return true;
+    } catch (Exception e) {
+      sender.sendMessage("Error: usage: /delbusstop <route> <stop>");
+    }
+    return false;
+  }
+}
